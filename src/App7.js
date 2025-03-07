3d87d9fc386c362f256a1231819d32ad16df85d7
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-03-07 19:49:42 +0530

    new

--- src/App7.js
+++ src/App7.js
@@ -1,35 +1,53 @@
 import React from "react";
 import { useState } from "react";
+import "./App7.css"
 export default function App6() {
-  const [item, setItem] = useState([]);
-  const [price, setPrice] = useState([]);
-  const [quantity, setQuantity] = useState([]);
-  const [text, setText] = useState();
+  const [items, setItems] = useState([]); // Initialized with an empty array
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
   const handleSubmit = () => {
-    setItem([...item, text]);
+    const total = price * quantity;
+    setItems([...items, { name, price, quantity, total }]);
+    setName("");
+    setPrice("");
+    setQuantity("");
+  
   };
   
   return (
-    <div>
+    <div className="container">
+    <div className="left-div">
         <h2>SUPER MARKET</h2>
       <p><input
         type="text"
         placeholder="Item name"
-        onChange={(event) => setText(event.target.value)}
+        onChange={(event) => setName(event.target.value)}
       ></input></p>
       <p><input
         type="text"
         placeholder="Price"
-        onChange={(event) => setText(event.target.value)}
+        onChange={(event) => setPrice(event.target.value)}
       ></input></p>
       <p><input
         type="text"
         placeholder="Quantity"
-        onChange={(event) => setText(event.target.value)}
+        onChange={(event) => setQuantity(event.target.value)}
       ></input></p>
       <button onClick={handleSubmit}>Add</button>
-      <hr></hr>
-      {item && item.map((value, index) => <li key={index}>{value}</li>)}
+    </div>
+    <div className="vertical-line" ></div>
+    <div className="right-div">
+      <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} , Price: {item.price} , Quantity: {item.quantity} => Total Cost:{" "}
+              {item.total}
+            </li>
+          ))}
+        </ul>
+    </div>
     </div>
   );
 }
